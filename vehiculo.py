2899e29b286cbbc5c679370b611f0ec7399454dd
Ivan Montes de Oca <ivanmontesdeoca00@icloud.com>
2026-07-28 20:29:20 -0400

    Fix clase transporte

--- vehiculo.py
+++ vehiculo.py
@@ -41,7 +41,7 @@ class Vehiculo:
     
     def mostrar_ficha_tecnica(self):
         print("\n" + "="* 50)
-        print(" --------------------- FICHA TECNICA DEL VEHICULO")
+        print(" --------------------- FICHA TECNICA DEL VEHICULO ------------------")
         print("=" * 50)
         print(f"Marca y Modelo:  {self.marca} {self.modelo}")
         print(f"Motor : {self.tipo_motor}")
@@ -134,3 +134,31 @@ class Camion(Vehiculo):
         tarifa_total = (self.get_precio_diario() * dias) + impuesto_carga_pesada
         detalle = print(f"Alquiler base {dias} dias: ${self.get_precio_diario() * dias:.2f} + Impuestos de carga pesada: ${impuesto_carga_pesada:.2f} ")
         return tarifa_total, detalle
+
+
+class Transporte(Vehiculo):
+    modelos_catalogo = {
+        "Mercedes Benz Sprinter": {
+            "marca": "Mercedes Benz", "precio_diario": 190.0, "capacidad_personas": 16,
+            "tipo_traccion": "4x2", "rendimiento_km": 9.0, "tipo_motor": "2.0L CDI"
+
+        }
+        "Hyundai H1 BUS": {
+            "marca": "Hyundai", "precio_diario": 185.0, "capacidad_personas": 19,
+            "tipo_traccion": "4x3", "rendimiento_km": 11.0
+        }
+    }
+
+    def __init__(self,modelo="Mercedes Benz Sprinter"):
+        if modelo not in self.modelos_catalogo:
+            modelo = list(self.modelos_catalogo.keys())[0]
+        
+        m= self.modelos_catalogo[modelo]
+        super().__init__(m["marca"], modelo, m["precio_diario"], m["capacidad_personas"], m["tipo_traccion"], "Traslado de gente", m["rendimiento_km"], m["tipo_motor"])
+
+    
+    def calcular_cotizacion(self,dias, cantidad_pasajeros= 1, **asd)
+        tarifa_total = (self.get_precio_diario() * dias) + cantidad_pasajeros
+        detalle = print(f"Alquiler base {dias} dias: $ {self.get_precio_diario() * dias:.2f} + Tasa por la cantidad de pasajeros: $ {cantidad_pasajeros}")
+        return tarifa_total, detalle
+        
